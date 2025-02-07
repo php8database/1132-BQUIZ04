39d2d83c42088792e915ed364496108d882cd7ee
php8database <php8database@gmail.com>
2025-02-07 16:26:43 +0800

    建立資料庫db07_04.sql

---
+++ db07_04.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2025-02-07 09:25:06
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `db07_04`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `acc` text NOT NULL,
+  `pw` text NOT NULL,
+  `pr` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `admins`
+--
+
+INSERT INTO `admins` (`id`, `acc`, `pw`, `pr`) VALUES
+(1, 'admin', '1234', 'a:5:{i:0;i:1;i:1;i:2;i:2;i:3;i:3;i:4;i:4;i:5;}');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `members`
+--
+
+CREATE TABLE `members` (
+  `id` int(10) UNSIGNED NOT NULL COMMENT '流水號',
+  `name` text NOT NULL COMMENT '姓名',
+  `acc` text NOT NULL COMMENT '帳號',
+  `pw` text NOT NULL COMMENT '密碼',
+  `addr` text NOT NULL COMMENT '地址',
+  `email` text NOT NULL COMMENT '電子郵件',
+  `tel` text NOT NULL COMMENT '電話',
+  `regdate` timestamp NOT NULL DEFAULT current_timestamp() COMMENT '新增註冊日期'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `members`
+--
+
+INSERT INTO `members` (`id`, `name`, `acc`, `pw`, `addr`, `email`, `tel`, `regdate`) VALUES
+(1, 'mem01', 'mem01', '1234', '', '', '', '2025-02-07 08:24:20');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `members`
+--
+ALTER TABLE `members`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
