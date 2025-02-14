cd2524b53685006d92fd28138c230c846817a5ac
php8database <php8database@gmail.com>
2025-02-14 15:53:01 +0800

    更新資料庫

--- db07_04.sql
+++ db07_04.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-02-13 09:13:58
+-- 產生時間： 2025-02-14 08:51:44
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -64,6 +64,40 @@ INSERT INTO `bottom` (`id`, `bottom`) VALUES
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(10) UNSIGNED NOT NULL COMMENT '流水號',
+  `no` text NOT NULL COMMENT '商品編號',
+  `name` text NOT NULL COMMENT '商品名稱',
+  `price` int(10) NOT NULL COMMENT '商品單價',
+  `stock` int(10) NOT NULL COMMENT '庫存量',
+  `spec` text NOT NULL COMMENT '規格',
+  `intro` text NOT NULL COMMENT '商品簡介',
+  `img` text NOT NULL COMMENT '商品圖片',
+  `big` int(10) NOT NULL COMMENT '大分類',
+  `mid` int(10) NOT NULL COMMENT '中分類',
+  `sh` int(1) NOT NULL COMMENT '是否上架'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `items`
+--
+
+INSERT INTO `items` (`id`, `no`, `name`, `price`, `stock`, `spec`, `intro`, `img`, `big`, `mid`, `sh`) VALUES
+(1, '010203', '手工訂製長夾', 1200, 2, '全牛皮', '手工製作長夾卡片層6*2 鈔票層 *2 零錢拉鍊層 *1\n採用愛馬仕相同的雙針縫法,皮件堅固耐用不脫線\n材質:直革鞣(馬鞍皮)牛皮製作\n手工染色', '0403.jpg', 1, 1, 1),
+(2, '020705', '兩用式磁扣腰包', 685, 18, '中型', '材質:進口牛皮\n顏色:黑色荔枝紋+黑色珠光面皮(黑色縫線)\n尺寸:15cm*14cm(高)*6cm(前後)\n產地:臺灣', '0404.jpg', 1, 1, 1),
+(3, '020706', '超薄設計男士長款真皮', 800, 61, 'L號', '基本:編織皮革對摺長款零錢包\n特色:最潮流最時尚的單品\n顏色:黑色珠光面皮(黑色縫線)\n形狀:黑白格編織皮革對摺', '0405.jpg', 1, 1, 1),
+(4, '030103', '經典牛皮少女帆船鞋', 1000, 6, 'S號', '以傳統學院派風格聞名，創始近百年工藝製鞋精神\n共用獨家專利氣墊技術，兼具紐約工藝精神，與舒適跑格靈魂', '0406.jpg', 2, 2, 1),
+(5, '030203', '經典優雅時尚流行涼鞋', 2650, 8, 'LL', '優雅流線方型楦頭設計，結合簡潔線條綴飾，\n獨特的弧度與曲線美，突顯年輕優雅品味，\n是年輕上班族不可或缺的鞋款！\n全新美國運回，現貨附鞋盒', '0407.jpg', 2, 3, 1),
+(6, '040202', '寵愛天然藍寶女戒', 28000, 1, '1克拉', '◎典雅設計品味款\n◎藍寶為珍貴天然寶石之一，具有保值收藏\n◎專人設計製造，以貴重珠寶精緻鑲工製造', '0408.jpg', 3, 4, 1),
+(7, '050107', '反折式大容量手提肩背包', 888, 15, 'L號', '特色:反折式的包口設計,釘釦的裝飾,讓簡單的包型更增添趣味性\n材質:棉布\n顏色:藍色\n尺寸:長50cm寬20cm高41cm\n產地:日本', '0409.jpg', 4, 1, 1),
+(8, '060108', '男單肩包男', 650, 7, '多功能', '特色:男單肩包/電腦包/公文包/雙肩背包多用途\n材質:帆布\n顏色:黑色\n尺寸:深11cm寬42cm高33cm\n產地:香港', '0410.jpg', 4, 1, 1);
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `members`
 --
 
@@ -102,7 +136,7 @@ CREATE TABLE `types` (
 --
 
 INSERT INTO `types` (`id`, `name`, `big_id`) VALUES
-(1, '流行皮件', 0),
+(1, '', 0),
 (2, '流行鞋區', 0),
 (3, '流行飾品', 0),
 (4, '背包', 0),
@@ -111,7 +145,7 @@ INSERT INTO `types` (`id`, `name`, `big_id`) VALUES
 (7, '少女鞋區', 2),
 (10, '紳士流行鞋區', 2),
 (11, '時尚手錶', 3),
-(12, '時尚珠寶', 3),
+(12, '', 3),
 (13, '背包', 4);
 
 --
@@ -128,6 +162,12 @@ ALTER TABLE `admins`
 -- 資料表索引 `bottom`
 --
 ALTER TABLE `bottom`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `items`
+--
+ALTER TABLE `items`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -157,6 +197,12 @@ ALTER TABLE `admins`
 --
 ALTER TABLE `bottom`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=9;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `members`
