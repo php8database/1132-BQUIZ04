550c1e9ad7133e6e99d75bf4a090535cccca235a
php8database <php8database@gmail.com>
2025-02-17 14:10:33 +0800

    顯示購物車商品內容

--- front/buycart.php
+++ front/buycart.php
@@ -13,3 +13,37 @@ if(!isset($_SESSION['Mem'])){
 
 ?>
 <h2 class="ct"><?=$_SESSION['Mem'];?>的購物車</h2>
+
+<table class="all">
+    <tr class="tt ct">
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>數量</td>
+        <td>庫存量</td>
+        <td>單價</td>
+        <td>小計</td>
+        <td>刪除</td>
+    </tr>
+    <?php 
+    foreach($_SESSION['cart'] as $id => $qt):
+        $item=$Item->find($id);
+    ?>
+    <tr class="pp">
+        <td class="ct"><?=$item['no'];?></td>
+        <td ><?=$item['name'];?></td>
+        <td class="ct"><?=$qt;?></td>
+        <td class="ct"><?=$item['stock'];?></td>
+        <td class="ct"><?=$item['price'];?></td>
+        <td class="ct">
+            <?php
+                echo $item['price']*$qt;
+            ?>
+        </td>
+        <td class="ct">
+            <img src="./icon/0415.jpg" alt="">
+        </td>
+    </tr>
+    <?php
+    endforeach;
+    ?>
+</table>
