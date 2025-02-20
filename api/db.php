c0a37be6a6d87270bd2098634d304df4a7b38e4b
php8database <php8database@gmail.com>
2025-02-20 09:57:55 +0800

    完成新增訂單功能

--- api/db.php
+++ api/db.php
@@ -4,7 +4,7 @@ session_start();
 
 
 class DB{
-protected $dsn="mysql:host=localhost;charset=utf8;dbname=db18";
+protected $dsn="mysql:host=localhost;charset=utf8;dbname=db07_04";
 protected $pdo;
 protected $table;
 
@@ -109,7 +109,7 @@ function to($url){
 }
 
 function q($sql){
-    $dsn="mysql:host=localhost;charset=utf8;dbname=db18";
+    $dsn="mysql:host=localhost;charset=utf8;dbname=db07_04";
     $pdo=new PDO($dsn ,'root','');
     return $pdo->query($sql)->fetchALL();
 }
